635696753819add7ea3735a52beedf6abf1ce6d0
Narva Siddhartha <116857953+Narvasiddhartha@users.noreply.github.com>
2025-05-25 17:53:43 +0530

    Updates

---
+++ precache-manifest.2e2f81063f002aa869c771b74f529a52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1e905ed1f19cadd59e9f346b9566a3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "78a55f10703162d80562",
+    "url": "/portfolio/static/css/main.1b5b6d08.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/portfolio/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "70c068b2fb9d959ad245",
+    "url": "/portfolio/static/js/3.730e5367.chunk.js"
+  },
+  {
+    "revision": "78a55f10703162d80562",
+    "url": "/portfolio/static/js/main.8ad4c2da.chunk.js"
+  },
+  {
+    "revision": "942b3624d771dd960b08",
+    "url": "/portfolio/static/js/runtime~main.400d7f1e.js"
+  }
+]);
